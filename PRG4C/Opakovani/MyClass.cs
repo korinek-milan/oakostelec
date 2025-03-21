da1ecbf29b46925547be9be3a38be0a061cf3e6b
korinek-milan <korinek-milan@seznam.cz>
2025-03-21 08:26:16 +0100

    Opakovani
    string builder + override ToString()

---
+++ PRG4C/Opakovani/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opakovani
+{
+    internal class MyClass
+    {
+        string name;
+        int age;
+        public string myMethod()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("abc");
+            sb.Append("de");
+            sb.AppendLine("f");
+
+            return sb.ToString();
+        }
+
+        public string myABC()
+        {
+            return "Abc";
+        }
+
+        //pokud nepoužívám override ToString(), můžu si udělat prostě svoji vypisovací metodu
+        public string getInfo()
+        {
+            return name + " " + age.ToString();
+        }
+
+        //vlastní ToString - abych mohl používat zažitou metodu ToString()
+        public override string ToString()
+        {
+            return name + " " + age.ToString();
+        }
+    }
+}
---
+++ PRG4C/Opakovani/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opakovani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Zadej hodnotu pro uložení do databáze");
+            int a;
+
+            string myStr = "11";
+            //dokud není splněná podmínka (číslo, které musí být 10), nech uživatele zadávat hodnotu
+            while (!int.TryParse(Console.ReadLine(), out a) && a == 10)
+            {
+                Console.WriteLine("je to špatně, zkus to znova");
+            }
+
+            //string builder pro práci se stringem
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("abc");
+            sb.Append("de");
+            sb.AppendLine("f");
+
+            //string builder je celá třída, kterou nemohu jen tak vypsat, proto potřebuji převést na string
+            Console.WriteLine(sb.ToString());
+
+            //moje instance
+            MyClass m = new MyClass();
+            //volání metod
+            Console.WriteLine(m.myMethod());
+            Console.WriteLine(m.getInfo());
+
+            //do mé třídy jsem si přidal override ToString, takže mohu volat ToString na celou instanci (ale to není samozřejmost)
+            Console.WriteLine(m.ToString());
+
+        }
+    }
+}
