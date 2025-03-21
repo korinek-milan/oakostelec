0f9964d83fc5c519d351b8608a87ca8514224b07
korinek-milan <korinek-milan@seznam.cz>
2025-03-21 08:16:41 +0100

    pohyb hlavy + jídlo

--- Krouzek/Python/Snake/Main.py
+++ Krouzek/Python/Snake/Main.py
@@ -1,52 +1,78 @@
 import turtle
+import time
+import random
 
-def bg(r: float, g: float, b: float):
-    print(r, g, b)
-
+delay = 0.1
 
 #herni okno
 wn = turtle.Screen()
 wn.title("Moje ošklivá hra")
-bg(r = 26, b = 80, g = 2)
-#wn.bgcolor(r=0.1, g=0.2, b=1.0)toto nefunguje proč??? -> není takto definovaná funkce při prokliku
+wn.bgcolor(0, 1, 0.4)#toto nefunguje proč??? -> není takto definovaná funkce při prokliku
 
 #wn.setup(800, 600)
-wn.setup(width=900, height=600)
-#wn.tracer(0)#netusim proc, ale musi to tam byt???
+wn.setup(width=800, height=600)
+wn.tracer(0)#netusim proc, ale musi to tam byt???
 
 
 #objekt zelva - objekt se spoustou vlastnosti jako napr. pohyb, tvar atd
 snakeHead = turtle.Turtle()
-snakeHead.shape("turtle")
+snakeHead.shape("square")
 snakeHead.goto(0,0)
-snakeHead.shapesize(5,5,0)
+snakeHead.penup()
+snakeHead.color("black")
+snakeHead.speed(0)
+snakeHead.direction = "right" #stop, up, down, left, right
 
-#snakeHead.color("black")
+
+
+food_x = random.randint((int)(-wn.window_width() / 2), (int) (wn.window_width() / 2)) #
+food_y = random.randint((int)(-wn.window_height() / 2), (int) (wn.window_height() / 2))
+#objekt pro jídlo (jablko?)
+food = turtle.Turtle()
+food.shape("circle")
+food.penup()
+food.goto(food_x,food_y)
+food.color("red")
+food.speed(0)
+
 #snakeHead.direction = "stop"
-#snakeHead.speed(0)
 
-#prekresleni pixelu obrazovky
-wn.update()
+def move():
+    if snakeHead.direction == "up": #když je směr nastavneý na up
+        y = snakeHead.ycor() #zjistí se y souřadnice
+        snakeHead.sety(y + 20) #změní se na stejnou hodnotu + 20 (20 proto, že je to defaultní velikost shape, tzn. posouváme celý shape)
+    if snakeHead.direction == "down":
+        y = snakeHead.ycor()
+        snakeHead.sety(y - 20)
+    if snakeHead.direction == "right":
+        x = snakeHead.xcor()
+        snakeHead.setx(x + 20)
+    if snakeHead.direction == "left":
+        x = snakeHead.xcor()
+        snakeHead.setx(x - 20)
+
+def up():
+    snakeHead.direction = "up"
+
+def down():
+    snakeHead.direction = "down"
+
+def right():
+    snakeHead.direction = "right"
+
+def left():
+    snakeHead.direction = "left"
+
+wn.listen()
+wn.onkeypress(up, "w") 
+wn.onkeypress(down, "s") 
+wn.onkeypress(left, "a") 
+wn.onkeypress(right, "d") 
+
 
 #hlavni smycka, kterou nakonec nevyuzijeme, ale nyni ji potrebujeme, aby se nam okno nezavrelo
-wn.mainloop()
-
-
-
-
-
-
-#wn = turtle.Screen()
-#wn.title("Snake Game")
-#wn.bgcolor("green")
-#wn.setup(width=600, height=600)
-#wn.tracer(0) # Turns off the screen updates
-
-
-'''head = turtle.Turtle()
-head.speed(0)
-head.shape("square")
-head.color("black")
-head.penup()
-head.goto(0,0)
-head.direction = "stop"'''
+#wn.mainloop()
+while True:
+    move()
+    wn.update() #prekresleni pixelu obrazovky
+    time.sleep(delay) #zpomalím
