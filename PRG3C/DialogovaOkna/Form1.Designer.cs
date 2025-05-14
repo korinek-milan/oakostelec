56baf5424fdf4ea3c0621fb69dcf24961efd5225
korinek-milan <41016431+korinek-milan@users.noreply.github.com>
2025-05-14 12:27:49 +0200

    dialogova okna

---
+++ PRG3C/DialogovaOkna/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DialogovaOkna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nacist = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Nacist
+            // 
+            this.Nacist.Location = new System.Drawing.Point(468, 123);
+            this.Nacist.Name = "Nacist";
+            this.Nacist.Size = new System.Drawing.Size(75, 23);
+            this.Nacist.TabIndex = 0;
+            this.Nacist.Text = "Nacist";
+            this.Nacist.UseVisualStyleBackColor = true;
+            this.Nacist.Click += new System.EventHandler(this.Nacist_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(306, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ulozit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Nacist);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Nacist;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PRG3C/DialogovaOkna/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DialogovaOkna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Nacist_Click(object sender, EventArgs e)
+        {
+            //chci zobrazit okno a dále pracovat pouze, pokud uživatel klikne na OK
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //z dialogu dostanu cestu, kterou uživatel vybral
+                string path = openFileDialog1.FileName;
+
+                //dále pracuji stejně jako když ukládám text v konzoli
+                string loadedText = File.ReadAllText(path);
+                textBox1.Text = loadedText;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //chci zobrazit okno a dále pracovat pouze, pokud uživatel klikne na OK
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //z dialogového okna získám cestu + název souboru a do souboru uložím, co je v textboxu
+                File.WriteAllText(saveFileDialog1.FileName, textBox1.Text);
+            }
+        }
+    }
+}
