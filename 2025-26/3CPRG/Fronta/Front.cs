4a9a32bb661cf4c53879f49bd8e7b227fbe99eb4
korinek-milan <41016431+korinek-milan@users.noreply.github.com>
2026-01-15 09:00:43 +0100

    fronta

---
+++ 2025-26/3CPRG/Fronta/Front.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fronta
+{
+    internal class Front
+    {
+        public Node Head;
+        public void Push(int value)
+        {
+            Node n = new Node(value);
+
+            if (Head == null)
+            {
+                Head = n;
+                return;
+            }
+
+            Node tmp = Head;
+            while (tmp.Next != null)
+            {
+                tmp = tmp.Next;
+            }
+
+            tmp.Next = n;
+        }
+
+        public Node Pop()
+        {
+            if (Head == null)
+            {
+                return null;
+            }
+
+            Node oldHead = Head;
+            Head = oldHead.Next;
+
+            return oldHead;
+        }
+
+        public int? Peek()
+        {
+            return Head.Value;
+        }
+    }
+}
