50a81e4ecd9f2c7767b1741e71ada5b8cc9976eb
korinek-milan <41016431+korinek-milan@users.noreply.github.com>
2026-04-30 09:47:14 +0200

    commit

---
+++ 2025-26/2CPRG/TryCatch/Kalkulacka.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    internal class Kalkulacka
+    {
+        public int deleni(int x, int y)
+        {
+            if (x > 10)
+            {
+                return x / y;
+            }
+            else
+            {
+                //Kalkulacka k = new Kalkulacka();
+                //Exception E = new Exception("Požadavek na první číslo je, aby bylo větší než 10");
+                //throw E;
+
+                //Random r = new Random();
+                //r.Next();
+
+                //int z = new Random().Next();
+                
+
+
+                throw new Exception("Požadavek na první číslo je, aby bylo větší než 10");
+            }
+
+        }
+    }
+}
---
+++ 2025-26/2CPRG/TryCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulacka k = new Kalkulacka();
+
+            try
+            {
+                Console.WriteLine("Zadej dvě hodnoty pro dělení a první hodnota musí být větší než 10");
+                Console.WriteLine(k.deleni(10, 2));
+                //int[] pole = new int[10];
+                //Console.WriteLine(pole[15]);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Nastala chyba v dělení nulou. Druhá zadaná hodnota nesmí být nula.");
+                Console.WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("zadal jste větší množství hodnot než je požadováno");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nastala neočekávaná chyba");
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.WriteLine("toto se vypíše na konci");
+        }
+    }
+}
